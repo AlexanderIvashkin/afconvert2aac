54678003e00d1d5a0c221fa57b7cdf45367a3f10
AlexanderIvashkin <alexander.ivashkin@icloud.com>
2019-05-17 01:21:52 +0200

    Fixed ffmpeg issue with non-existent video stream (i.e. album covers)

--- m4a2aac.sh
+++ m4a2aac.sh
@@ -6,7 +6,7 @@ mkdir -p aac
 for src in *.m4a; do
     dst="tmp/$src"
     afconvert -f m4af -d aac -v -s 3 -q 72 "$src" "$dst"
-    ffmpeg -i "$src" -i "$dst" -map 0:v -c:v copy -map 1:a -c:a copy -map_metadata 0 "aac/$src"
+    ffmpeg -i "$src" -i "$dst" -map 0?:v -c:v copy -map 1:a -c:a copy -map_metadata 0 "aac/$src"
 done
 
 rm -r tmp
